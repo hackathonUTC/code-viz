3cdbf0872f04b287ac78b80ee5529a5e631a6d77
Aliaume MOREL <aliaume.morel@gmail.com>
2015-11-29 00:15:57 +0100

    Fix class names query + fix bad registering order for DataModel

--- main.cpp
+++ main.cpp
@@ -9,9 +9,9 @@ int main(int argc, char *argv[])
     QGuiApplication app(argc, argv);
 
     QQmlApplicationEngine engine;
+    qmlRegisterSingletonType<DataModel>("codeviz", 1, 0, "DataModel", DataModel::qml_datamodel_singleton_callback);
     engine.load(QUrl(QStringLiteral("qrc:/main.qml")));
 
-    qmlRegisterSingletonType<DataModel>("codeviz", 1, 0, "DataModel", DataModel::qml_datamodel_singleton_callback);
 
     return app.exec();
 }
